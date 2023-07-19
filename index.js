f49d5364f0b45071ee749f4d731cf6490a3f7dfc
anayet ullah <anayetdev@gmail.com>
2023-07-19 17:11:27 +0600

    Get api added to get particualr house info to be updated

--- index.js
+++ index.js
@@ -121,6 +121,14 @@ app.post("/house-owner/addHouse", async (req, res) => {
   
   });
 
+  app.get("/house-owner/myHouses/:id", async (req, res) => {
+	const id = req.params.id;
+	const query = { _id: new ObjectId(id) };
+	const result = await houseCollection.findOne(query);
+	res.send(result);
+  });
+  
+
 app.get("/", (req, res) => {
   res.send("Server is working");
 });
